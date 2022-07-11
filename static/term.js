9abb54a45af496f8c7dd99d76ca50d0614d01869
Khalil Estell <kammcecorp@gmail.com>
2022-07-10 20:22:08 -0700

    Change monospace font

--- static/term.js
+++ static/term.js
@@ -7,18 +7,15 @@ let term = new Terminal({
   cursorBlink: true,
   lineHeight: 1,
   fontSize: 18,
-  fontFamily: "Ubuntu Mono, courier-new, courier, monospace",
-  scrollback: 1024*100,
+  fontFamily: "Andale Mono, courier-new, courier, monospace",
+  scrollback: 1024 * 100,
 });
 
 term.on('key', function (key, event) {
   key = (event.code == "Backspace") ? "\b" : key;
   key = (event.code == "Enter") ? "\n" : key;
   const encoder = new TextEncoder("utf-8");
-  serial_extension.postMessage({
-    "command": "write",
-    "data": Array.from(encoder.encode(key))
-  });
+  // TODO: Use web serial here
 });
 
 term.on('data', function (data, ev) {
